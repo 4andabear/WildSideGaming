152fd4d53621fc4397cc87a1cee31c3b64ee6efd
nshunk <troop67.nathanshunk@gmail.com>
2017-05-08 08:57:38 -0400

    Today, I have started making java files, so i can implement a live stream function with the app.  I plan to have the layout of it done, but I am not sure if the live streaming function will work anytime soon.

---
+++ app/src/main/java/org/pltw/example/wildsidegaming/LiveFeed.java
@@ -0,0 +1,33 @@
+package org.pltw.example.wildsidegaming;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by nshunk on 5/8/2017.
+ */
+
+public abstract class LiveFeed extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        }
+}
